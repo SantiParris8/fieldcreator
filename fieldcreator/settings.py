e9aad37aee28e8880b68824d0965e6575c1760f2
SantiParris8 <32781893+SantiParris8@users.noreply.github.com>
2024-08-03 21:28:13 -0300

    Update settings.py

--- fieldcreator/settings.py
+++ fieldcreator/settings.py
@@ -106,7 +106,7 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 SALESFORCE_CONSUMER_KEY = os.environ.get('SALESFORCE_CONSUMER_KEY')
 SALESFORCE_CONSUMER_SECRET = os.environ.get('SALESFORCE_CONSUMER_SECRET')
-SALESFORCE_REDIRECT_URI = 'https://fieldcreator.herokuapp.com/oauth_response'
+SALESFORCE_REDIRECT_URI = 'https://fieldcreator.onrender.com/oauth_response'
 SALESFORCE_API_VERSION = int(os.environ.get('SALESFORCE_API_VERSION', 55))
 
 # EMAIL SERVER SETTINGS
